9d2dc26b11eab5d61d5b98396aaa206af4044047
thorgal333 <francedigitale@gmx.fr>
2017-02-25 09:59:38 +0100

    test console

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //dsdqsd
+            Console.WriteLine("toto");
         }
     }
 }
